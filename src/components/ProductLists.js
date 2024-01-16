8e21bd770375cc8a6ec783fef12bcb97d6559c15
Abdelkarim Sadek <Abdelkarim.sadek@outlook.com>
2024-01-16 11:25:02 +0200

    last commit

--- src/components/ProductLists.js
+++ src/components/ProductLists.js
@@ -1,22 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Product from "./Product";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
 
 const ProductLists = () => {
   const apiUrl = "https://fakestoreapi.com/products";
 
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
-  useEffect(() => {
-    AOS.init({
-      duration: 1000, // Animation duration in milliseconds
-      offset: 200,    // Offset (in pixels) from the top of the document at which the animation should trigger
-      easing: 'ease-in-out', // Easing type
-    });
-  }, []);
-
   const getProducts = () => {
     fetch(apiUrl)
       .then((res) => res.json())
@@ -29,10 +18,9 @@ const ProductLists = () => {
   };
 
   const getProductsInCategory = (catName) => {
-      fetch(`${apiUrl}/category/${catName}`)
-        .then((res) => res.json())
-        .then((data) => setProducts(data));
-    
+    fetch(`${apiUrl}/category/${catName}`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
   };
   useEffect(() => {
     getProducts();
@@ -42,8 +30,17 @@ const ProductLists = () => {
     <div>
       <h2 className="mt-4 mb-2 text-center fw-bold">Products</h2>
       <div className="container">
-        <div className="d-flex justify-content-end align-items-center buttons" style={{ width: "100%" }}>
-        <button className="btn btn-secondary filter" onClick={() => getProducts()}>All</button>
+        <div
+          className="d-flex justify-content-end align-items-center buttons"
+          style={{ width: "100%" }}
+          data-aos="fade-right"
+        >
+          <button
+            className="btn btn-secondary filter"
+            onClick={() => getProducts()}
+          >
+            All
+          </button>
           {categories.map((cat, ind) => {
             return (
               <button
@@ -51,7 +48,7 @@ const ProductLists = () => {
                 className="btn btn-secondary filter"
                 style={{ margin: "15px 5px 25px" }}
                 onClick={() => {
-                    getProductsInCategory(cat);
+                  getProductsInCategory(cat);
                 }}
               >
                 {cat}
@@ -61,7 +58,7 @@ const ProductLists = () => {
         </div>
         <div className="row">
           {products.map((product, ind) => {
-            return <Product key={ind} {...product}/>;
+            return <Product key={ind} {...product} />;
           })}
         </div>
       </div>
